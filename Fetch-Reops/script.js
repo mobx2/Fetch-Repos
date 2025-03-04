57c3bfe194e1017ef498872af8cfa51f2dd1a9d3
Ibraheem Shaheen <143117266+mobx2@users.noreply.github.com>
2025-03-04 09:23:13 +0200

    "Completed the project

--- Fetch-Reops/script.js
+++ Fetch-Reops/script.js
@@ -16,3 +16,61 @@ getButton.onclick = function () {
 
 // Get Repos Function
 
+function getRepos() {
+  if (theInput.value == "") {
+    reposData.innerHTML = "Please Inter Your Github UserName";
+  } else {
+    fetch(`https://api.github.com/users/${theInput.value}/repos`)
+      .then((response) => {
+        // document.querySelector(".no-data").style.display = "none";
+        return response.json();
+      })
+      .then((repos) => {
+        setTimeout(() => {
+          for (let i = 0; i < repos.length; i++) {
+            // Create Repo Name Span
+            let repoBox = document.createElement("div");
+            repoBox.classList.add("repo-box");
+
+            // Create Repo Name TextNode
+            let repoName = document.createTextNode(repos[i].name);
+
+            repoBox.appendChild(repoName);
+
+            // Create Btns Box
+
+            let btnsDiv = document.createElement("div");
+            btnsDiv.classList.add("btns-div");
+            repoBox.appendChild(btnsDiv);
+
+            // Create Repo Name Anchor
+            let repoAnchor = document.createElement("a");
+            repoAnchor.classList.add("repo-link");
+            repoAnchor.innerHTML = "Visit";
+
+            // Create Repo Link href
+            repoAnchor.href = `${repos[i].html_url}`;
+
+            // Append Repo Anchor To Repo Span
+
+            btnsDiv.appendChild(repoAnchor);
+
+            // Create Stars Count
+
+            let starsCount = document.createElement("span");
+            starsCount.classList.add("stars-count");
+            let starsCountText = document.createTextNode(
+              `Stars ${repos[i].stargazers_count}`
+            );
+
+            starsCount.appendChild(starsCountText);
+
+            btnsDiv.appendChild(starsCount);
+
+            reposData.appendChild(repoBox);
+          }
+        }, 1000);
+      });
+  }
+  document.querySelector(".no-data").innerHTML = "";
+}
